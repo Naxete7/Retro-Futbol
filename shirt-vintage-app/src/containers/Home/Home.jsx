1fbe4979c5746664958ec7825402773a3450bab4
Ignacio <naxete7@hotmail.es>
2023-05-29 17:05:34 +0200

    resolve error in home

--- shirt-vintage-app/src/containers/Home/Home.jsx
+++ shirt-vintage-app/src/containers/Home/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Home.scss";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 
 
 
+const home= () =>{
+    
+    return (
+        <Container>
+            <Row>
+                <Col></Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default home
