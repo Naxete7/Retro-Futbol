ae9c7d8aa82bffca61f0d7545c2b0c697c47d378
Ignacio <naxete7@hotmail.es>
2023-07-04 18:38:13 +0200

    ok

--- shirt-vintage-app/src/containers/Home/Home.jsx
+++ shirt-vintage-app/src/containers/Home/Home.jsx
@@ -12,7 +12,7 @@ const home= () =>{
         <Container>
             <Row>
                 <Col>
-                    <Col></Col>
+                    
                 </Col>
             </Row>
         </Container>
