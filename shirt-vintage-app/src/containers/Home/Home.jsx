1f52f6a2b1c63508be02f950f2aeb4c114d7fcc4
Ignacio <naxete7@hotmail.es>
2023-07-05 17:41:54 +0200

    home error

--- shirt-vintage-app/src/containers/Home/Home.jsx
+++ shirt-vintage-app/src/containers/Home/Home.jsx
@@ -12,7 +12,7 @@ const home= () =>{
         <Container>
             <Row>
                 <Col>
-                    
+                
                 </Col>
             </Row>
         </Container>
