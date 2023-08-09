2109e4764109be0779a4dee30fc2db0e7c8330fb
Ignacio <naxete7@hotmail.es>
2023-08-09 19:57:18 +0200

    first image create

--- shirt-vintage-app/src/components/Header/Header.jsx
+++ shirt-vintage-app/src/components/Header/Header.jsx
@@ -7,11 +7,10 @@ function ColorSchemesExample() {
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/shirts">Camisetas</Nav.Link>
+           
           </Nav>
         </Container>
       </Navbar>
--- shirt-vintage-app/src/containers/Shirts/Shirts.jsx
+++ shirt-vintage-app/src/containers/Shirts/Shirts.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import "./Shirts.scss"
+import ford from "../../assets/img/ford.jpg"
+
 
 const Shirts = () => {
-    return ( 
-        <div className="shirtsDesign">Soy Camisetas</div>
-    )
+    return (
+      <div className="shirtsDesign">
+        <img src={ford} alt="ford" />
+      </div>
+    );
 }
 
 export default Shirts;
