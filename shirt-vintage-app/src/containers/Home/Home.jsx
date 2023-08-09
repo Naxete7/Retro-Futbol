520ac372c55b45ffb07063ee145828eb9959b4fa
Ignacio <naxete7@hotmail.es>
2023-08-09 20:38:25 +0200

    add images

--- shirt-vintage-app/src/containers/Home/Home.jsx
+++ shirt-vintage-app/src/containers/Home/Home.jsx
@@ -1,18 +1,58 @@
 import React from "react";
 import "./Home.scss";
+import { Container, Col, Row } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+import ford from "../../assets/img/ford.jpg";
+import liv1960 from "../../assets/img/liv1960.jpg"
 
-
-
-
-
-
-const home= () =>{
+function IndividualIntervalsExample() {
+  return (
+    <Container
+      fluid
+      className="teamDesign d-flex flex-wrap  align-items-center justify-content-center"
+    >
+      <Row className="carouselDesign d-flex justify-content-center mb-5">
+        <Col className="carouselDesign w-80 ">
+          <Carousel className="carouselDesign d-flex justify-content-center">
+            <Carousel.Item interval={1000}>
+              <img
+                className="d-block w-100 d-flex justify-item-center"
+                src={ford}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h2>Camisetas Vintage</h2>
+                <p>Disfruta de las mejores camisetas de la historia</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+              <img
+                className="d-block w-100 d-flex justify-item-center"
+                src={liv1960}
+                alt="Second slide"
+              />
+              <Carousel.Caption className="">
+                <h2>Camisetas Vintage</h2>
+                <p>Disfruta de las mejores camisetas de la historia</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 d-flex justify-item-center"
+                src={1991}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h2>Camisetas Vintage</h2>
+                <p>Disfruta de las mejores camisetas de la historia</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
     
-    return (
-        <div className="homeDesign">Soy el Home</div>
-        
-    );
-    
+    </Container>
+  );
 }
 
-export default home;
+export default IndividualIntervalsExample;
